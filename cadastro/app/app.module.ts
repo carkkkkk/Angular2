175233c226e98e85a74eee2d3eec6ae325e95ee8
Trevizan <Felipe.Trevizan@btgpactual.com>
2017-11-23 13:11:52 -0200

    Desenvolver Insert

---
+++ cadastro/app/app.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { ClientesModule } from './clientes/clientes.module';
+import { AppRoutingModule} from './app-routing.module';
+
+@NgModule({
+    imports : [AppRoutingModule, BrowserModule, ClientesModule],
+    declarations :[AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ cadastro/app/clientes/cliente-detalhe.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params} from '@angular/router';
+import { Location } from '@angular/common';
+import { Cliente } from './cliente.model';
+import { ClienteService } from './cliente.service';
+
+@Component({
+    moduleId: module.id,
+    selector : 'cliente-detalhe',
+    templateUrl : 'cliente-detalhe.component.html'    
+})
+export class ClienteDetalheComponent implements OnInit {
+    cliente: Cliente;
+
+    constructor (
+        private clienteService: ClienteService,
+        private route: ActivatedRoute,
+        private location: Location
+    ){}
+
+    ngOnInit(): void {
+        this.cliente = new Cliente(0, '', '', '')
+
+        this.route.params.forEach((params: Params)=>{
+            let id: number = +params['id'];
+
+            if (id){
+                this.clienteService.getCliente(id)
+                .then((cliente: Cliente)=>{
+                    console.log(cliente)
+                    this.cliente = cliente
+                })
+            }
+        })
+    }
+
+    getFormGroupClass(isValid: boolean, isPristine: boolean): {} {
+        return {
+            'form-group': true,
+            'has-danger': !isValid && !isPristine,
+            'has-success': isValid && !isPristine
+        }
+    }
+
+    getFormControlClass(isValid: boolean, isPristine: boolean): {} {
+        return {
+            'form-control': true,
+            'has-danger': !isValid && !isPristine,
+            'has-success': isValid && !isPristine
+        }
+    }
+}
